8bf0e0dbd47ca6290d78bfd3b63134aa8e683bdb
proHong <tjdgus0454@gmail.com>
2022-11-03 07:41:29 +0900

    eureka 서비스 리팩토링, user 서비스 생성

--- eureka-service/src/test/java/com/example/eurekaservice/EurekaServiceApplicationTests.java
+++ eureka-service/src/test/java/com/example/eurekaservice/EurekaServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.ecommerce;
+package com.example.eurekaservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EcommerceApplicationTests {
+class EurekaServiceApplicationTests {
 
     @Test
     void contextLoads() {
