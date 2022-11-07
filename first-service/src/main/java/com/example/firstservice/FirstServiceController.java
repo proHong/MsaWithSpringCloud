e67decf3920952a15ac3e1891369365cec512724
proHong <tjdgus0454@gmail.com>
2022-11-07 21:54:08 +0900

    gateway filter 추가

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,14 +1,27 @@
 package com.example.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the first service!";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Welcome to the first service!";
+    }
+    @GetMapping("/check")
+    public String check() {
+        return "Check to the first service!";
     }
 }
