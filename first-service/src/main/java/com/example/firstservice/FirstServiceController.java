39ffcb8410917a7468e9bcc935aa6520039a34a9
proHong <tjdgus0454@gmail.com>
2022-12-14 07:39:42 +0900

    add LB

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,15 +1,25 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the first service!";
@@ -21,7 +31,8 @@ public class FirstServiceController {
         return "Welcome to the first service!";
     }
     @GetMapping("/check")
-    public String check() {
-        return "Check to the first service!";
+    public String check(HttpServletRequest request) {
+        log.info("Server port = {} ", request.getServerPort());
+        return String.format("Hi, there. This is a message from First Service on PORT %s", env.getProperty("local.server.port"));
     }
 }
